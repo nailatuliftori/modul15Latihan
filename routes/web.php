00f2250d2cbdb4568e83ed31b9923fc6432d6838
nailatuliftori <nailatuliftori@student.telkomuniversity.ac.id>
2024-12-13 19:54:21 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('home', [HomeController::class, 'index'])->name('home');
+
+Route::get('profile', ProfileController::class)->name('profile');
+
+Route::resource('employees', EmployeeController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('auth');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
